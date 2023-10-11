d90c252b18c900f6168713cc847bac45d7c8795a
FabricioDE <fabriciolucaspst@gmail.com>
2023-10-11 10:11:07 -0300

    reload off

--- main.py
+++ main.py
@@ -11,4 +11,4 @@ app.include_router(ar.router, tags=['positive'])
 
 if __name__ == '__main__':
 
-    uvicorn.run("main:app", host="0.0.0.0", port=8003, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8003)
